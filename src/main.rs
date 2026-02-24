8b2d5cf475b5220ad7a006610438b80464fa9c5e
Redas Jefisovas <114405740+holoflash@users.noreply.github.com>
2026-02-24 22:30:05 +0100

    feat: replace default icon

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,8 @@ fn main() -> eframe::Result<()> {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([1100.0, 700.0])
             .with_min_inner_size([800.0, 500.0])
-            .with_title("TRAKATUI"),
+            .with_title("TRAKATUI")
+            .with_icon(make_icon()),
         ..Default::default()
     };
 
@@ -54,5 +55,42 @@ impl eframe::App for TrakatuiApp {
         if self.app.playing {
             ctx.request_repaint();
         }
+    }
+}
+
+fn make_icon() -> egui::IconData {
+    let size = 32u32;
+    let mut rgba = vec![0u8; (size * size * 4) as usize];
+
+    let bg = [18u8, 18, 24, 255];
+    let fg = [80u8, 200, 220, 255];
+
+    for pixel in rgba.chunks_exact_mut(4) {
+        pixel.copy_from_slice(&bg);
+    }
+
+    let set = |rgba: &mut [u8], x: u32, y: u32, color: &[u8; 4]| {
+        let i = ((y * size + x) * 4) as usize;
+        rgba[i..i + 4].copy_from_slice(color);
+    };
+
+    // top bar of T: row 6-9, cols 6-25
+    for y in 6..10 {
+        for x in 6..26 {
+            set(&mut rgba, x, y, &fg);
+        }
+    }
+
+    // vertical stem of T: rows 10-25, cols 14-17
+    for y in 10..26 {
+        for x in 14..18 {
+            set(&mut rgba, x, y, &fg);
+        }
+    }
+
+    egui::IconData {
+        rgba,
+        width: size,
+        height: size,
     }
 }
